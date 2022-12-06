e58fe273b7ad39825108fdb89a6b3ab5a271ded5
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-12-06 15:52:05 +0530

    Refacoting the code by adding param-contructor and deleting the param of method

--- src/com/bridgelabz/mood_analyzer_project/MoodAnalyzer.java
+++ src/com/bridgelabz/mood_analyzer_project/MoodAnalyzer.java
@@ -1,24 +1,48 @@
 package com.bridgelabz.mood_analyzer_project;
 
 public class MoodAnalyzer {
+	String message;
+
+//Default constructor
+	public MoodAnalyzer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// param-constructor
+	public MoodAnalyzer(String message) {
+		super();
+		this.message = message;
+	}
+
 	public static void main(String[] args) {
+
 		// Master
 		System.out.println("Welcome to Mood Analyzer Program");
 
 		// UC1: Object of the Mood Analyzer and calling method analysis Mood
-		MoodAnalyzer analyzer = new MoodAnalyzer();
+		// MoodAnalyzer analyzer = new MoodAnalyzer();
 
 		// TC1.1: Given “I am in Sad Mood” message Should Return SAD
-		String mood1 = analyzer.analyseMood("This is SAD Message");
+		// String mood1 = analyzer.analyseMood("This is SAD Message");
 
 		// TC1.2: Given “I am in Any Mood” message Should Return HAPPY
-		String mood2 = analyzer.analyseMood("This is ANY Message");
-		System.out.println("Mood is: " + mood1);
-		System.out.println("Mood is: " + mood2);
+		// String mood2 = analyzer.analyseMood("This is ANY Message");
+		// System.out.println("Mood is: " + mood1);
+		// System.out.println("Mood is: " + mood2);
+
+		// Refactor: the code to use param-constructor to call and getting output
+		// TC1.1: Given “I am in Sad Mood” message Should Return SAD
+		MoodAnalyzer analyzer = new MoodAnalyzer("This is SAD Message");
+		System.out.println(analyzer.analyseMood());
+
+		// TC1.2: Given “I am in Any Mood” message Should Return HAPPY
+		MoodAnalyzer analyzer1 = new MoodAnalyzer("This is ANY Message");
+		System.out.println(analyzer1.analyseMood());
 	}
 
-	public String analyseMood(String string) {
-		if (string.toUpperCase().contains(("SAD"))) {
+	public String analyseMood() {
+		if (message.toUpperCase().contains(("SAD"))) {
 			return "SAD";
 		} else {
 			return "HAPPY";
