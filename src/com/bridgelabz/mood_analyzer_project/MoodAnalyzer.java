b174fabf7d87bd41ccb542e4fc76fa0ddff2dc89
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-12-06 14:40:30 +0530

    Initializing the Mood Analyzer program

---
+++ src/com/bridgelabz/mood_analyzer_project/MoodAnalyzer.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.mood_analyzer_project;
+
+public class MoodAnalyzer {
+public static void main(String[] args) {
+	System.out.println("Welcome to Mood Analyzer Program");
+}
+}
